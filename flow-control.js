2d9500d99f481987ad1014e78c3200866de2eba5
Samantha Copland <sam.copland1992@gmail.com>
2020-05-07 20:33:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,9 +16,24 @@ function ternaryTeenager(age) { return age>= 13 && age<=19 ? 'You are a teenager
 
 function switchAge(age)
 {switch(age) 
-  {case age<=19 && age>=13:
+  {case 13:
     return 'You are a teenager';
   break;
+  case 14:
+  return 'You are a teenager';
+  break;
+  case 15:
+  return 'You are a teenager';
+  break;
+  case 16:
+  return 'You are a teenager';
+  break;
+  case 17:
+  return 'You are a teenager';
+  break;
+  case 18: 'You are a teenager';
+  break;
+  case 19: 'You are a teenager';
   default:
   return 'You have an age';}
 
